d01de373d8e7e86a33d8dfa9ab13f673e5ef5db7
Doully <themeanyhead@gmail.com>
2017-12-14 19:52:24 +0000

    update comment in user model

--- models/user.js
+++ models/user.js
@@ -6,7 +6,7 @@ var userSchema = new mongoose.Schema({
     password: String
 });
 
-//adds a bunch of functions from passportLocalMongoose into userSchema model
+//adds functions from passportLocalMongoose into userSchema model
 userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", userSchema);
